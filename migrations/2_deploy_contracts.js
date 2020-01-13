b5fba2cd544cb71028cc513878067426cec72b04
Rodimiro Cerrato E <rodyce@gmail.com>
2020-01-12 23:49:12 -0600

    Use distrib contract address in ERC20 contract
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,16 +2,9 @@ const CBETDistributionContract = artifacts.require('./CBETDistribution.sol')
 const CBETTokenContract = artifacts.require('./CBETToken.sol');
 
 
-if (process.argv.length < 4) {
-    console.error('Need to specify address for token distribution contract');
-    process.exit(1);
-}
-
-const targetAddress = process.argv[3];
-
-console.log(`Deploying to address ${targetAddress}`);
-
 module.exports = function(deployer) {
-    deployer.deploy(CBETDistributionContract);
-    deployer.deploy(CBETTokenContract, targetAddress);
+    deployer.deploy(CBETDistributionContract)
+    .then(function() {
+        return deployer.deploy(CBETTokenContract, CBETDistributionContract.address);
+    });
 }
